93b14201f2e8189319c4f8d8d8249b278eb9b600
Henrique Ferreira <henriqueanalista.ads@gmail.com>
2026-04-20 14:03:07 -0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   vite: {
-    base: "/controle-financeiro/",
+    // Definindo como '/' para funcionar no Cloudflare
+    base: "/",
   },
 });
